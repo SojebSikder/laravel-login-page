75af9892ee4f9252888067623991150319a2ee23
sojebsikder <sojebsikder@gmail.com>
2024-12-03 22:58:57 +0600

    fixed bugs

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 
 Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login');
