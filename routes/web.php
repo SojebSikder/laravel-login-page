25e4e3aab8d87dc09204a965fb27745de31305e4
sojebsikder <sojebsikder@gmail.com>
2024-12-03 22:54:28 +0600

    fixed bugs

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@ Route::get('/', function () {
 
 Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login');
 Route::post('/auth/register', [AuthController::class, 'register'])->name('auth.register');
-Route::get('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
+Route::post('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
 Route::resource('user', UserController::class);
